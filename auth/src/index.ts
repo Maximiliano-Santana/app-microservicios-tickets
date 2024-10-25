16dc7c7a5279eb4b6e16acf08fc0c2112788b2ad
Maximiliano Santana <72675327+Maximiliano-Santana@users.noreply.github.com>
2024-10-25 17:57:34 -0600

    Iniciando docker y kubernetes

---
+++ auth/src/index.ts
@@ -0,0 +1,11 @@
+// const express = require('express')
+// const json = require('body-parser')
+import { json } from 'body-parser'
+import express  from 'express';
+
+const app = express();
+app.use(json());
+
+app.listen(3000, ()=>{
+    console.log('Listening on port 3000')
+});
